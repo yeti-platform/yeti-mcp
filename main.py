c23a11a269b6595aa8166ec1cfd6bed6ef64a0f8
Thomas Chopitea <tomchop@gmail.com>
2025-06-04 15:19:14 +0000

    Add more functions

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from contextlib import asynccontextmanager
 from dataclasses import dataclass
 
 from mcp.server.fastmcp import Context, FastMCP
-from yeti.api import YetiApi
+from yeti.api import YetiApi, SUPPORTED_IOC_TYPES
+
 from typing import Any
 
 
@@ -62,6 +63,17 @@ def _get_timesketch_client():
     return ctx.request_context.lifespan_context.ts_client
 
 
+@mcp.resource("yeti://supported_ioc_types")
+def get_supported_ioc_types() -> list[str]:
+    """
+    Get the list of supported IOC types.
+
+    Returns:
+        A list of supported IOC types.
+    """
+    return SUPPORTED_IOC_TYPES
+
+
 @mcp.tool()
 def match_observables(observables: list[str]) -> list:
     """
@@ -76,6 +88,66 @@ def match_observables(observables: list[str]) -> list:
     client = _get_yeti_client()
     results = client.match_observables(observables)
     return results
+
+
+@mcp.tool()
+def add_observables_bulk(
+    observables: list[dict[str, str]],
+    tags: list[str] | None = None,
+) -> list:
+    """
+    Add observables to Yeti.
+
+    All tags will be applied to all observables, so use with caution. If different
+    tags are needed for each observable, use several calls to `add_observables_bulk` instead.
+
+    Args:
+        observables: A list of observables to add. Observables should be dictionaries
+            with keys like "type" and "value", e.g. [{"type": "ip", "value": "8.8.8.8"}].
+            A list of valid types can be found in the `get_supported_ioc_types` resource.
+        tags: A list of tags to associate with the observables. Only use
+            high-relevance tags related to the malicious nature of the observables.
+            (e.g. malware name, role of the IOC (C2, persistence, staging, etc.))
+
+    Returns:
+        A list of added observables.
+    """
+    client = _get_yeti_client()
+    results = client.add_observables_bulk(observables, tags=tags)
+    return results
+
+
+@mcp.tool()
+def link_objects(
+    source: dict[str, Any],
+    target: dict[str, Any],
+    link_type: str = "related",
+    description: str | None = None,
+) -> dict:
+    """
+    Link two objects in Yeti.
+
+    As a general rule, if linking Observables (e.g. hashes, IP addresses, domains),
+    to Entities (e.g. threat actors, malware, incidents, intrusion sets), use the
+    obseravble as a source and a relevant description, such as "used by" or "seen in".
+
+    A link with the same source, target and link_type will be overwritten.
+
+    e.g. 8.8.8.8 (source) -> threat actor (target) with link_type "related" and description "used by".
+    <hash> (source) -> <malware> (target) with link_type "related" and description "seen in".
+
+    Args:
+        source: The source object.
+        target: The target object.
+        link_type: The type of link to create (default is "related").
+        description: An optional description for the link.
+
+    Returns:
+        A dictionary containing the details of the created link.
+    """
+    client = _get_yeti_client()
+    result = client.link_objects(source, target, link_type, description)
+    return result
 
 
 @mcp.tool()
@@ -167,7 +239,11 @@ def search_indicators(
 
 @mcp.tool()
 def get_neighbors(
-    source: str, target_types: list[str] | None = None, count: int = 10, page: int = 0
+    source: str,
+    target_types: list[str] | None = None,
+    direction: str = "outbound",
+    count: int = 10,
+    page: int = 0,
 ) -> list:
     """
     Get neighbors of an object by searching the Yeti graph. This can be
@@ -176,6 +252,7 @@ def get_neighbors(
 
     Args:
         source: The <type>/ID notation of the source object for which to find neighbors, e.g. entities/1234.
+        dierction: The direction of the search, either "inbound" or "outbound" or "any".
         target_types: A list of target object (entities or indicators) types to filter neighbors by.
         count: The max number of results to return.
         page: The page number for pagination.
@@ -189,6 +266,7 @@ def get_neighbors(
         source,
         target_types=target_types,
         include_original=False,
+        direction=direction,
         count=count,
         page=page,
     )
@@ -229,6 +307,7 @@ async def search_timesketch_events(
     filter_return_fields: list[str] | None = None,
     max_events: int | None = None,
     sort: str = "asc",
+    starred: bool = False,
 ) -> list[dict[str, Any]]:
     """
     Returns a list of event dictionaries (limited by max_events, if provided).
@@ -257,6 +336,7 @@ async def search_timesketch_events(
             "datetime, message, data_type, tag, yara_match, sha256_hash".
         max_events: Optional maximum number of events to return. If None, returns all matching events.
         sort: Sort order for datetime field, either "asc" or "desc". Default is "asc".
+        starred: If True, only return starred events. If False, return all events.
 
     Returns:
         A list of dictionaries representing the events found in the sketch.
@@ -279,6 +359,11 @@ async def search_timesketch_events(
         search_instance.order_descending()
     else:
         search_instance.order_ascending()
+
+    if starred:
+        star_chip = search.LabelChip()
+        star_chip.use_star_label()
+        search_instance.add_chip(star_chip)
 
     result_df = search_instance.table
 
